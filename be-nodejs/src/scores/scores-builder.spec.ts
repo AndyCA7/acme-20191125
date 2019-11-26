6264a82c5d9de5bfeca489aaa6de97a2ec0d1ba3
Bryan LaFavors <blafavors@gmail.com>
2019-11-26 09:57:24 -0500

    semicolon cleanup

--- be-nodejs/src/scores/scores-builder.spec.ts
+++ be-nodejs/src/scores/scores-builder.spec.ts
@@ -59,7 +59,7 @@ describe('ScoresBuilder', () => {
         it('should return 10 0s when 20 0 rolls are given', () => {
             var rollScores: number[] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
             expect(scoresBuilder.buildScore(rollScores)).toEqual([0,0,0,0,0,0,0,0,0,0]);
-        })
+        });
 
         it('should return 2,4,6,...,20 when 20 1 rolls are given', () => {
             var rollScores: number[] = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
@@ -69,7 +69,7 @@ describe('ScoresBuilder', () => {
         it('should return 12,13,14.....14 when a 10,1,1 followed by only 0s is the given score', () => {
             var rollScores: number[] = [10,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
             expect(scoresBuilder.buildScore(rollScores)).toEqual([12,14,14,14,14,14,14,14,14,14]);
-        })
+        });
 
         it('should return 11,12,12...12 when 1,9,1, followed by only 0s is the given score', () => {
             var rollScores: number[] = [1,9,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
@@ -79,7 +79,7 @@ describe('ScoresBuilder', () => {
         it('should return a final score of 300 for a perfect game of all 10s', () => {
             var rollScores: number[] = [10,10,10,10,10,10,10,10,10,10,10,10];
             expect(scoresBuilder.buildScore(rollScores)).toEqual([30, 60, 90, 120, 150, 180, 210, 240, 270, 300]);
-        })
+        });
 
         it('should return 12 if only a spare is scored in the 10th frame with a single 1 roll afterward', () => {
             var rollScores: number[] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,8,1];
@@ -89,7 +89,7 @@ describe('ScoresBuilder', () => {
         it('should return 100 if every other ball is a gutter and spare', () => {
             var rollScores: number[] = [0, 10,0, 10,0, 10,0, 10,0, 10,0, 10,0, 10,0, 10,0, 10,0, 10, 0];
             expect(scoresBuilder.buildScore(rollScores)).toEqual([10, 20, 30, 40, 50, 60, 70, 80, 90, 100]);
-        })
+        });
 
     });
 
